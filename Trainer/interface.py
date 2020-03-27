b001abf9f5a5e6bfa3c60aee8c4a47e9cb19b778
SamuelBFG <samuelbfgomes@gmail.com>
2020-03-27 18:42:53 -0300

    hotkeys interface

--- Trainer/interface.py
+++ Trainer/interface.py
@@ -11,34 +11,50 @@ class Application:
 
         self.img = ImageTk.PhotoImage(Image.open(os.path.join(os.path.dirname(__file__), 'imgs', 'img.png')))
         img_label = Button(image=self.img)
-        img_label.grid(row=0, column=0)
+        img_label.grid(row=0, column=0, columnspan=4)
 
         self.titulo = Label(text="HOTKEYS")
         self.titulo["font"] = ("Arial", "10", "bold")
-        self.titulo.grid(row=1, column=0, padx=60)
+        self.titulo.grid(row=1, column=0, sticky='nesw', columnspan=4)
 
         self.hotkeys = Label(text="SCREENSHOT")
         self.hotkeys["font"] = ("Arial", "10", "bold")
-        self.hotkeys.grid(row=3, column=3)
+        self.hotkeys.grid(row=4, sticky='nesw', columnspan=4)
 
         # self.space = Label(text='            ')
         # self.space.grid(row=0, column=1)
   
         self.foodLabel = Label(text="Food: ", font=self.fontePadrao, anchor="e")
-        self.foodLabel.grid(row=3, column=0, columnspan=4)
+        self.foodLabel.grid(sticky='e', row=2, column=0)
   
         self.food = Entry()
-        self.food["width"] = 10
+        self.food["width"] = 5
         self.food["font"] = self.fontePadrao
-        self.food.grid(row=2, column=1)
+        self.food.grid(sticky='w', row=2, column=1)
   
-        self.softLabel = Label(text="Senha ", font=self.fontePadrao)
-        self.softLabel.grid(row=1, column=3)
+        self.softLabel = Label(text="Soft: ", font=self.fontePadrao)
+        self.softLabel.grid(sticky='e',row=2, column=2)
   
         self.soft = Entry()
-        self.soft["width"] = 10
+        self.soft["width"] = 5
         self.soft["font"] = self.fontePadrao
-        self.soft.grid(row=1, column=4)
+        self.soft.grid(sticky='w',row=2, column=3)
+
+        self.ringLabel = Label(text="Ring: ", font=self.fontePadrao)
+        self.ringLabel.grid(sticky='e', row=3, column=0)
+  
+        self.ring = Entry()
+        self.ring["width"] = 5
+        self.ring["font"] = self.fontePadrao
+        self.ring.grid(sticky='w', row=3, column=1)
+
+        self.runeLabel = Label(text="Rune: ", font=self.fontePadrao)
+        self.runeLabel.grid(sticky='e',row=3, column=2)
+  
+        self.rune = Entry()
+        self.rune["width"] = 5
+        self.rune["font"] = self.fontePadrao
+        self.rune.grid(sticky='w', row=3, column=3)
   
         self.autenticar = Button()
         self.autenticar["text"] = "Autenticar"
@@ -47,7 +63,7 @@ class Application:
         self.autenticar["command"] = self.verificaSenha
   
         self.mensagem = Label(text="", font=self.fontePadrao)
-        self.mensagem.grid(row=0, column=0)
+        self.mensagem.grid(row=5, column=0)
   
     #Método verificar senha
     def verificaSenha(self):
